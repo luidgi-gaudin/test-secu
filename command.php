0d807d4081f2bff2ea0c72efcd5fd8d5483613ab
luidgi <146814537+luidgi-gaudin@users.noreply.github.com>
2025-03-25 14:58:07 +0100

    erg

--- command.php
+++ command.php
@@ -1,7 +1,9 @@
 <?php
 if (isset($_GET['ip'])) {
     $ip = $_GET['ip'];
+    echo"ip : ". $ip . "<br>";
     $command = shell_exec("ping -c 1". $ip);
+    echo "commande exécutée : <br>";
     echo "<pre>" . $command . "</pre>";
 }else{
     echo 'No IP';
